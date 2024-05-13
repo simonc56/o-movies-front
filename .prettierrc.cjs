6792d632160213ae8ec287c91ab49107885a7ada
simonc56 <simchat@gmail.com>
2024-05-13 14:20:24 +0200

    chore: mise en place de la structure avec vite

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf',
+  trailingComma: true,
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'preserve',
+  tabWidth: 2,
+  printWidth: 120,
+};
