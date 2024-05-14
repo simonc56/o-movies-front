44a570d3829ded569427f12d918c04238e4866b8
Sebiiih <desreumaux.sebastien@gmail.com>
2024-05-14 14:02:02 +0200

    start of form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import SignupPage from '../SignupPage/SignupPage';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" />
+        <Route path='/inscription' element={<SignupPage />}/>
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/SignupPage/SignupPage.tsx
@@ -0,0 +1,63 @@
+import { ChangeEvent, useState } from 'react';
+import { Birthday } from './Birthday';
+import './SignupPage.scss';
+
+function SignupPage() {
+  const [username, setUsername] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // API , Base de donnée ??!! 
+    
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="form-title">FORMULAIRE D'INSCRIPTION A O'MOVIES</h1>
+      <input
+        type="text"
+        placeholder="Votre nom"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+      <input
+        type="email"
+        placeholder="Votre E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Pays"
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Ville"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        required
+      />
+      <button type="submit">S'inscrire</button>
+    </form>
+  );
+};
+
+
+export default SignupPage;
