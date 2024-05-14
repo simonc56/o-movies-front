0d91c3ae10fa0ec5efa1a5b2e6a6190771f6c9a3
Sebiiih <desreumaux.sebastien@gmail.com>
2024-05-14 16:13:49 +0200

    break

--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -1,5 +1,4 @@
 import { ChangeEvent, useState } from 'react';
-import { Birthday } from './Birthday';
 import './SignupPage.scss';
 
 function SignupPage() {
@@ -12,54 +11,65 @@ function SignupPage() {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    // API , Base de donnée ??!! 
-    
+    // API , Base de donnée ??!!
   };
 
   return (
     <section className="signup-section">
-    <form onSubmit={handleSubmit}>
-      <h1 className="form-title">FORMULAIRE D'INSCRIPTION A O'MOVIES</h1>
-      <input
-        type="text"
-        placeholder="Votre nom"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required
-      />
-      <input
-        type="email"
-        placeholder="Votre E-mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Mot de passe"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Pays"
-        value={country}
-        onChange={(e) => setCountry(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Ville"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        required
-      />
-      <button type="submit">S'inscrire</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <h1 className="form-title">FORMULAIRE D'INSCRIPTION A O'MOVIES</h1>
+        <input
+          type="text"
+          placeholder="Votre nom"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="birthday">Date de naissance:</label>
+        <input
+          id="birthday"
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+        <input type="text" 
+        placeholder="Pays" 
+        value={country} 
+        onChange={(e) => setCountry(e.target.value)} 
+        required 
+        />
+        <input type="text" 
+        placeholder="Ville" 
+        value={city} 
+        onChange={(e) => setCity(e.target.value)} 
+        required 
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Choissisez un mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirmer votre mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">S'inscrire</button>
+      </form>
     </section>
   );
-};
-
+}
 
 export default SignupPage;
