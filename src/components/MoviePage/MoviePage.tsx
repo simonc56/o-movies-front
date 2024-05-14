118bcf21055074e8e569138d11c5095d70559e0d
simonc56 <simchat@gmail.com>
2024-05-14 10:22:08 +0200

    feat: ajout de la MoviePage

---
+++ src/components/MoviePage/MoviePage.tsx
@@ -0,0 +1,14 @@
+function MoviePage() {
+  return (
+    <div>
+      <h1>Dune 2</h1>
+      <span>2024</span>
+      <p>
+        Paul Atréides se rallie à Chani et aux Fremen tout en préparant sa revanche contre ceux qui ont détruit sa
+        famille.{' '}
+      </p>
+    </div>
+  );
+}
+
+export default MoviePage;
