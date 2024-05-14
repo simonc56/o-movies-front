41dc035182aaffc306e0e6c143a5a72d3db3d6f0
simonc56 <simchat@gmail.com>
2024-05-14 10:38:54 +0200

    feat: ajout Routes dans App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" />
+      </Routes>
       <Footer />
     </div>
   );
