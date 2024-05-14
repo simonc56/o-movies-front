b44d9472c77f17f921ce54ddf9ca2f9ba87d619b
Sebiiih <desreumaux.sebastien@gmail.com>
2024-05-15 01:17:49 +0200

    css finish

--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -8,65 +8,99 @@ function SignupPage() {
   const [city, setCity] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // API , Base de donnée ??!!
+    if (password === confirmPassword) {
+      // API
+    } else {
+      // Erreur 
+      setPasswordsMatch(false);
+    }
   };
 
   return (
     <section className="signup-section">
       <form onSubmit={handleSubmit}>
-        <h1 className="form-title">FORMULAIRE D'INSCRIPTION A O'MOVIES</h1>
-        <input
-          type="text"
-          placeholder="Votre nom"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <label htmlFor="birthday">Date de naissance:</label>
-        <input
-          id="birthday"
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-        <input type="text" 
-        placeholder="Pays" 
-        value={country} 
-        onChange={(e) => setCountry(e.target.value)} 
-        required 
-        />
-        <input type="text" 
-        placeholder="Ville" 
-        value={city} 
-        onChange={(e) => setCity(e.target.value)} 
-        required 
-        />
-        <input
-          type="email"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Choissisez un mot de passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirmer votre mot de passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">S'inscrire</button>
+        <h1 className="form-title">INSCRIPTION A O'MOVIES</h1>
+        <div className="input-container"> 
+          <input
+            type="text"
+            className="signup-input signup-username"
+            placeholder="Votre nom"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="birthday" className="birthday-label">Date de naissance:</label>
+          <input
+            id="birthday"
+            className="signup-input signup-birthday"
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <input 
+            type="text" 
+            className="signup-input signup-country"
+            placeholder="Pays" 
+            value={country} 
+            onChange={(e) => setCountry(e.target.value)} 
+            required 
+          />
+        </div>
+        <div className="input-container"> 
+          <input 
+            type="text" 
+            className="signup-input signup-city"
+            placeholder="Ville" 
+            value={city} 
+            onChange={(e) => setCity(e.target.value)} 
+            required 
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="email"
+            className="signup-input signup-email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            className="signup-input signup-password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            className="signup-input signup-confirm-password"
+            placeholder="Confirmer votre mot de passe"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          {!passwordsMatch && <p className="error-message" >Les mots de passe ne correspondent pas</p>}
+        </div>
+        <div className="button-container"> 
+          <button type="submit">S'inscrire</button>
+        </div>
       </form>
     </section>
   );
