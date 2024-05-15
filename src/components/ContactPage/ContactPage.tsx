b454bfd664936d6aa0063dbbbc1e1ac00d0c8c24
Sebiiih <desreumaux.sebastien@gmail.com>
2024-05-16 01:46:56 +0200

    page advancement

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -1,56 +1,80 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import './ContactPage.scss';
-
 
 function ContactPage() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message: ''
+    message: '',
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // api ?!
 
     setFormData({
       name: '',
       email: '',
-      message: ''
+      message: '',
     });
   };
 
   return (
     <section className="contact-section">
-    <div className="ContactPage">
-      <h1 className= "contact-title" >Contactez-nous</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Votre nom :</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Votre E-mail :</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Quel est votre message :</label>
-          <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
-        </div>
-        <button className="contact-button" 
-        type="submit">Envoyer</button>
-      </form>
-    </div>
-     </section>
+      <div className="ContactPage">
+        <h1 className="contact-title">Contactez-nous</h1>        
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="nom-contact"
+              id="name"
+              name="name"
+              placeholder="Votre nom"
+              value={formData.name}
+              onChange={handleChange}              
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="email-contact"
+              id="email"
+              name="email"
+              placeholder="Votre E-mail"
+              value={formData.email}
+              onChange={handleChange}              
+              required
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              id="message"
+              className="message-contact"
+              name="message"
+              placeholder="Quel est votre message ?  :)"
+              value={formData.message}
+              onChange={handleChange}              
+              required
+            />
+          </div>
+          <div className="button-container-contact">
+          <button type="submit">
+            Envoyer
+          </button>
+          </div>
+        </form>
+        <p className="info-contact">Nous vous répondrons par e-mail dans les meilleurs délais.</p>
+      </div>
+    </section>
   );
 }
-
 export default ContactPage;
