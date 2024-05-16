2959fc931e497e343fbe013b47e71c0f201f3edf
simonc56 <simchat@gmail.com>
2024-05-16 14:37:04 +0200

    Header: utilisation de composant mantine

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,43 @@
+import { Burger, Container, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo-pop-corn.png';
 import LoginSignup from '../LoginSignup/LoginSignup';
 import Searchbar from '../Searchbar/Searchbar';
 import './Header.scss';
 
+const links = [
+  { link: '/actuellement', label: "A l'affiche" },
+  { link: '/prochainement', label: 'Prochainement' },
+  { link: '/films', label: 'Films' },
+  { link: '/playlists', label: 'Mes playlists' },
+];
+
 function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => {
+    return (
+      <NavLink key={link.label} to={link.link} className=".link" onClick={(event) => event.preventDefault()}>
+        {link.label}
+      </NavLink>
+    );
+  });
+
   return (
     <header className="header">
-      <NavLink to="/" className="title">
-        <img src={logo} alt="logo o'movies" />
-        <h1>O'movies</h1>
-      </NavLink>
-      <nav>
-        <ul className="header-links links">
-          <li>
-            <NavLink to="/actuellement">A l'affiche</NavLink>
-          </li>
-          <li>
-            <NavLink to="/prochainement">Prochainement</NavLink>
-          </li>
-          <li>
-            <NavLink to="/films">Films</NavLink>
-          </li>
-          <li>
-            <NavLink to="/playlists">Mes playlists</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Searchbar />
-      <LoginSignup />
+      <Container size="md">
+        <div className="title">
+          <img src={logo} alt="logo o'movies" />
+          <h1>O'movies</h1>
+        </div>
+        <Group gap={5} visibleFrom="sm">
+          {items}
+        </Group>
+        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+        <Searchbar />
+        <LoginSignup />
+      </Container>
     </header>
   );
 }
