03d0893592678229f86fd06ffc747e89e3eae4b2
Simon <simchat@gmail.com>
2024-05-16 18:12:27 +0200

    Add Mantine css framework (#9)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,27 @@
+import { MantineProvider } from '@mantine/core';
 import { Route, Routes } from 'react-router-dom';
 import ConnectionPage from '../ConnectionPage/ConnectionPage';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import SignupPage from '../SignupPage/SignupPage';
 import MoviePage from '../MoviePage/MoviePage';
+import SignupPage from '../SignupPage/SignupPage';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" />
-        <Route path='/connexion' element={<ConnectionPage />}/>
-        <Route path='/inscription' element={<SignupPage />}/>
-        <Route path="/films/:id" element={<MoviePage />} />
-      </Routes>
-      <Footer />
-    </div>
+    <MantineProvider>
+      <div className="App" data-mantine-color-scheme="light">
+        <Header />
+        <Routes>
+          <Route path="/" />
+          <Route path="/connexion" element={<ConnectionPage />} />
+          <Route path="/inscription" element={<SignupPage />} />
+          <Route path="/films/:id" element={<MoviePage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </MantineProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 
+import '@mantine/core/styles.css';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
