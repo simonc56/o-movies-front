b01c9617159d3d665bedd71bffceb2424fc7f32b
simonc56 <simchat@gmail.com>
2024-05-16 15:17:27 +0200

    feat: Header responsive avec menu burger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Burger, Container, Group } from '@mantine/core';
+import { Box, Burger, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo-pop-corn.png';
@@ -10,35 +10,39 @@ const links = [
   { link: '/actuellement', label: "A l'affiche" },
   { link: '/prochainement', label: 'Prochainement' },
   { link: '/films', label: 'Films' },
-  { link: '/playlists', label: 'Mes playlists' },
+  // { link: '/playlists', label: 'Mes playlists' },
 ];
 
 function Header() {
-  const [opened, { toggle }] = useDisclosure(false);
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
 
   const items = links.map((link) => {
     return (
-      <NavLink key={link.label} to={link.link} className=".link" onClick={(event) => event.preventDefault()}>
+      <NavLink key={link.label} to={link.link} className="link" onClick={(event) => event.preventDefault()}>
         {link.label}
       </NavLink>
     );
   });
 
   return (
-    <header className="header">
-      <Container size="md">
-        <div className="title">
-          <img src={logo} alt="logo o'movies" />
-          <h1>O'movies</h1>
-        </div>
-        <Group gap={5} visibleFrom="sm">
-          {items}
+    <Box size="md">
+      <header className="header">
+        <Group justify="space-between" h="100%">
+          <div className="title">
+            <img src={logo} alt="logo o'movies" />
+            <h1>O'movies</h1>
+          </div>
+          <Group gap={0} visibleFrom="md">
+            {items}
+          </Group>
+          <Group gap={0} visibleFrom="md">
+            <Searchbar />
+            <LoginSignup />
+          </Group>
+          <Burger opened={drawerOpened} onClick={toggleDrawer} size="md" hiddenFrom="md" color="yellow" />
         </Group>
-        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        <Searchbar />
-        <LoginSignup />
-      </Container>
-    </header>
+      </header>
+    </Box>
   );
 }
 
