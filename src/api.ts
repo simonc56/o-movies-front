f4ec4068b80f7e76a5ed2d5209dc572304d51811
simonc56 <simchat@gmail.com>
2024-05-17 08:10:25 +0200

    feat: add api fonctions to get backend data

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { LoginCredentials, SignupCredentials } from './@types/Credentials';
+
+const instanceAxios = axios.create({
+  baseURL: import.meta.env.VITE_API_LOGIN_URL,
+});
+
+export async function login(credentials: LoginCredentials) {
+  const response = await instanceAxios.post('/auth/login', credentials);
+  return response;
+}
+export async function signup(credentials: SignupCredentials) {
+  const response = await instanceAxios.post('/auth/signup', credentials);
+  return response;
+}
+
+export function addTokenJWTToAxiosInstance(token: string) {
+  instanceAxios.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
