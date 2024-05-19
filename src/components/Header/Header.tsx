0e9499a8326a135c32179ab15019b742a99e3dca
simonc56 <simchat@gmail.com>
2024-05-19 11:09:10 +0200

    refactor: remove useless return

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,21 +16,17 @@ const links = [
 function Header() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
 
-  const items = links.map((link) => {
-    return (
-      <NavLink key={link.label} to={link.link} className="link" onClick={(event) => event.preventDefault()}>
-        {link.label}
-      </NavLink>
-    );
-  });
+  const items = links.map((link) => (
+    <NavLink key={link.label} to={link.link} className="link" onClick={(event) => event.preventDefault()}>
+      {link.label}
+    </NavLink>
+  ));
 
-  const drawerItems = links.map((link) => {
-    return (
-      <NavLink key={link.label} to={link.link} className="drawerLink" onClick={(event) => event.preventDefault()}>
-        {link.label}
-      </NavLink>
-    );
-  });
+  const drawerItems = links.map((link) => (
+    <NavLink key={link.label} to={link.link} className="drawerLink" onClick={(event) => event.preventDefault()}>
+      {link.label}
+    </NavLink>
+  ));
 
   return (
     <Box size="md">
