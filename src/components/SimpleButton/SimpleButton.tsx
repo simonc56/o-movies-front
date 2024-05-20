2d868aea87f0b1d5142b94683f3af030d6275b59
simonc56 <simchat@gmail.com>
2024-05-20 21:59:50 +0200

    cleaner SimpleButton

--- src/components/SimpleButton/SimpleButton.tsx
+++ src/components/SimpleButton/SimpleButton.tsx
@@ -1,32 +1,19 @@
 import { Button } from '@mantine/core';
-import { useState } from 'react';
 
 type SimpleButtonProps = {
   label: string;
   // eslint-disable-next-line react/require-default-props
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
+  [key: string]: any;
 };
 
-function SimpleButton({ label, onClick, ...props }: SimpleButtonProps) {
-  const [isHovered, setIsHovered] = useState(false);
-
+function SimpleButton({ label, onClick, type = 'button', ...props }: SimpleButtonProps) {
   return (
-    <Button
-      onClick={onClick}
-      color={isHovered ? 'bg' : 'primary'}
-      autoContrast={!isHovered}
-      onMouseOver={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      {...props}
-    >
+    <Button color="primary" autoContrast type={type} {...props}>
       {label}
     </Button>
   );
 }
-
-SimpleButton.defaultProps = {
-  type: 'button',
-};
 
 export default SimpleButton;
