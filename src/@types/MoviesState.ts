f02be28129b47b5babe67e91d5bc959da4eff6b5
simonc56 <simchat@gmail.com>
2024-05-21 20:50:13 +0200

    add api response types

--- src/@types/MoviesState.ts
+++ src/@types/MoviesState.ts
@@ -9,6 +9,24 @@ export type SuccessOneMovieResponse = {
   data: MovieType;
 };
 
+// here I create a generic type for 'success' API responses (no export)
+// you have to specify the type of T when you want to reuse SuccessResponse<T>
+type SuccessResponse<T> = {
+  status: 'success';
+  data: T;
+};
+
+export type FailResponse = {
+  data: {
+    status: 'fail';
+    error: string;
+  };
+};
+
 export type SuccessReviewResponse = {
-  data: string;
+  data: SuccessResponse<{ review_id: number }>;
+};
+
+export type SuccessRatingResponse = {
+  data: SuccessResponse<{ rating_id: number }>;
 };
