925085b1bf95f73cd48e3e4b90490ea1401ee5dc
HugoLaboucheix <lastdragon@Last-Dragon>
2024-05-24 11:06:52 +0200

    Add of Mantine

--- src/components/ConnectionPage/ConnectionPage.tsx
+++ src/components/ConnectionPage/ConnectionPage.tsx
@@ -1,119 +1,121 @@
 import { ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { TextInput, PasswordInput, Tooltip, Center, Text, Group, Anchor, Checkbox, Button, rem } from '@mantine/core';
+import { IconInfoCircle } from '@tabler/icons-react';
 import { AxiosResponse } from 'axios';
 import { LoginCredentials } from '../../@types/Credentials';
 import { login } from '../../api';
 import './ConnectionPage.scss';
 
-
 interface LoginResponse {
   message: string;
-// If necessary, add other answer property
 }
 
 function ConnectionPage() {
-// State to store form field values
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
   const [stayConnected, setStayConnected] = useState(false);
-  // Status to manage loading status, errors and response messages
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [responseMessage, setResponseMessage] = useState<string | null>(null);
 
-// Change manager for the email field
   const emailChange = (event: ChangeEvent<HTMLInputElement>) => {
     setEmailValue(event.target.value);
   };
-// Change manager for password field
+
   const passwordChange = (event: ChangeEvent<HTMLInputElement>) => {
     setPasswordValue(event.target.value);
   };
-// Change manager for the "Stay connected" checkbox
+
   const stayConnectedChange = (event: ChangeEvent<HTMLInputElement>) => {
     setStayConnected(event.target.checked);
   };
-// Function to manage the submission of the login form
+
   const onLogin = async () => {
     setLoading(true);
     setError(null);
     setResponseMessage(null);
 
-// Create an object with user credentials
     const loginCredentials: LoginCredentials = {
       email: emailValue,
       password: passwordValue,
     };
 
     try {
-// API call to try to connect
       const response: AxiosResponse<LoginResponse> = await login(loginCredentials);
-// In case of success, uptade the response message
       setResponseMessage(response.data.message);
     } catch (err) {
-// Error update in case of connection failure
       setError("Impossible de vous connecter. Veuillez vérifier vos informations d'identification.");
     } finally {
       setLoading(false);
     }
   };
 
+  const rightSection = (
+    <Tooltip
+      label="We store your data securely"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: 'pop-bottom-right' }}
+    >
+      <Text component="div" c="dimmed" style={{ cursor: 'help' }}>
+        <Center>
+          <IconInfoCircle style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+
   return (
     <section className="connection-section">
-      <div>
+      <div className="card">
         <h1 className="connection-title">Connexion</h1>
         <p>Connectez-vous pour accéder à votre espace personnel.</p>
-        <form action="">
-          <div className="mail">
-            <input 
-              type="email" 
-              placeholder="exemple@domaine.com" 
-              value={emailValue} 
-              onChange={emailChange} 
-              required 
-            />
-          </div>
-          <div className="mdp">
-            <input 
-              type="password" 
-              placeholder="Mot de passe" 
-              value={passwordValue} 
-              onChange={passwordChange} 
-              required 
-            />
-          </div>
-          <div>
-            <input 
-              type="checkbox" 
-              id="checkbox" 
-              checked={stayConnected} 
-              onChange={stayConnectedChange} 
-            />
-            <label htmlFor="checkbox">Rester connecté</label>
-          </div>
+        <form className="connection-form">
+          <TextInput
+            label="Email"
+            placeholder="exemple@domaine.com"
+            value={emailValue}
+            onChange={emailChange}
+            rightSection={rightSection}
+            required
+            className="connection-input"
+          />
+
+          <PasswordInput
+            label="Mot de passe"
+            required
+            placeholder="Mot de passe"
+            value={passwordValue}
+            onChange={passwordChange}
+            className="connection-input"
+            mt="md"
+          />
+
+          <Checkbox label="Rester connecté" checked={stayConnected} onChange={stayConnectedChange} mt="md" />
           {error && <p className="error-message">{error}</p>}
           {responseMessage && <p className="response-message">{responseMessage}</p>}
-          <button 
-            className="button-connexion" 
-            type="button" 
-            onClick={onLogin} 
-            disabled={loading}
-          >
+          <Button type="button" onClick={onLogin} disabled={loading} fullWidth mt="md">
             {loading ? 'Loading...' : 'Connexion'}
-          </button>
+          </Button>
         </form>
-
-        <div className="link-signUp">
-          <p>Vous n'avez pas encore de compte?</p>
-          <Link to="/inscription" className="signup-link">
-            Créer un compte
-          </Link>
-        </div>
-        <div className="link-reinitiate">
-          <Link to="/réinitialisation-email" className="password-link">
-            Réinitialiser le mot de passe
-          </Link>
-        </div>
+        <Group mt="md">
+          <div className="link-container">
+            <Anchor
+              href="#"
+              onClick={(event) => event.preventDefault()}
+              pt={2}
+              fw={500}
+              fz="sm"
+              className="link-reinitiate"
+            >
+              Mot de passe oublié?
+            </Anchor>
+            <Anchor component={Link} to="/inscription" pt={2} fw={500} fz="sm" className="link-signUp">
+              Créer un compte
+            </Anchor>
+          </div>
+        </Group>
       </div>
     </section>
   );
