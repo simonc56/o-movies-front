bdfc4f2dc8efa3cbf85b74aa6fca09e81c9348f7
simonc56 <simchat@gmail.com>
2024-06-30 15:47:33 +0200

    add manualChunks in vite build config to reduce js file size

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,25 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mantine': [
+            '@mantine/core',
+            '@mantine/carousel',
+            '@mantine/dates',
+            '@mantine/form',
+            '@mantine/hooks',
+            '@mantine/modals',
+            '@mantine/notifications',
+          ],
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux'],
+          'icons': ['@tabler/icons-react', 'react-icons'],
+          'utils': ['axios', 'dayjs'],
+        },
+      },
+    },
   },
   css: {
     preprocessorOptions: {
