25d369621866e3b161ab1ef94587d81cd9a116e9
simonc56 <simchat@gmail.com>
2024-10-15 10:13:24 +0200

    fix: wrong path to poster placeholder

--- src/components/Homepage/PosterCarousel/PosterImage.tsx
+++ src/components/Homepage/PosterCarousel/PosterImage.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import noPoster from '../../../assets/no-poster.webp';
 
 function PosterImage({ title_fr, poster_path }: { title_fr: string; poster_path: string }) {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
     <div className="poster-container">
-      <div className={`poster-placeholder ${imageLoaded ? 'hidden' : ''}`} />
+      <div
+        className={`poster-placeholder ${imageLoaded ? 'hidden' : ''}`}
+        style={{ backgroundImage: `url(${noPoster})` }}
+      />
       <img
         width={230}
         src={poster_path}
