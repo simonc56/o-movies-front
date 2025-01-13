dbfec56557e5ff18e4e6bd0ee2b70bda3e2097ec
simonc56 <simchat@gmail.com>
2025-01-13 22:45:26 +0100

    alias for @tabler/icons-react because of bug with vite dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
   build: {
     sourcemap: true,
     rollupOptions: {
